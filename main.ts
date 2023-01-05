a456c63405c8d26c177a8617244214b889a7ca60
cnkacnj <122009022+cnkacnj@users.noreply.github.com>
2023-01-05 15:10:03 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+input.onButtonPressed(Button.A, function () {
+    item.move(1)
+})
+input.onButtonPressed(Button.B, function () {
+    item.turn(Direction.Right, 45)
+})
+let sprite = 0
+let item: game.LedSprite = null
+item = game.createSprite(0, 0)
+basic.forever(function () {
+    sprite = 0
+    if (sprite) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
